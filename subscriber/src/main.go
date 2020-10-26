09e12377cb247fb4b8f219d97f0f3db4f8a506d0
ralvescosta <rafael.rac.mg@gmail.com>
2020-10-26 19:09:59 -0300

    refactor: initial configs

--- subscriber/src/main.go
+++ subscriber/src/main.go
@@ -13,7 +13,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://rabbitmq:123456@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
